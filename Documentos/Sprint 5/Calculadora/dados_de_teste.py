0517616261ae078bff4a008c91ef8e3db91f3d66
Luis <luis.magris@gmail.com>
2025-05-16 15:48:39 -0300

    Conclusão das funções de testes restantes, implementação de parametrização e fixtures e criação de arquivo separado para os dados utilizados para o teste.

---
+++ Documentos/Sprint 5/Calculadora/dados_de_teste.py
@@ -0,0 +1,31 @@
+dados_soma = [
+    (2, 3, 5),
+    (5, 5, 10),
+    (20, 7, 27)
+]
+
+dados_subtracao = [
+    (2, 6, -4),
+    (6, 1, 5),
+    (8, 8, 0)
+]
+
+dados_multiplicacao = [
+    (5, 8, 40),
+    (9, 1, 9),
+    (76, 1, 76)
+]
+
+dados_divisao = [
+    (20, 5, 4),
+    (70, 35, 2),
+    (34, 0, None)
+]
+
+dados_potencia = [
+    (4, 2, 16)
+]
+
+dados_porcetagem = [
+    (20, 40, 8)
+]
--- Documentos/Sprint 5/Calculadora/test_calculadora.py
+++ Documentos/Sprint 5/Calculadora/test_calculadora.py
@@ -1,9 +1,32 @@
 import pytest
 from calculadora import Calculadora
+from dados_de_teste import *
 
 @pytest.fixture
-    def calculadora():
-        return Calculadora()
+def calculadora():
+    return Calculadora()
 
-def test_soma(calculadora):
-    assert calculadora.soma(2, 3) == 5
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_soma)
+def test_soma(a, b, resultado_esperado, calculadora):
+    assert calculadora.soma(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_subtracao)
+def test_subtracao(a, b, resultado_esperado, calculadora):
+    assert calculadora.sub(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_multiplicacao)
+def test_multiplicacao(a, b, resultado_esperado, calculadora):
+    assert calculadora.mult(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_divisao)
+def test_divisao(a, b, resultado_esperado, calculadora):
+    assert calculadora.div(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_potencia)
+def test_potencia(a, b, resultado_esperado, calculadora):
+    assert calculadora.pow(a, b) == resultado_esperado
+
+@pytest.mark.parametrize("a, b, resultado_esperado", dados_porcetagem)
+def test_porcentagem(a, b, resultado_esperado, calculadora):
+    assert calculadora.per(a, b) == resultado_esperado
